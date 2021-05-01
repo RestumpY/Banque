f4b9a1b10feafa7125aad8a401921e3af271f95d
RestumpY <70935893+RestumpY@users.noreply.github.com>
2021-05-01 18:09:43 +0200

    Initial commit

---
+++ Modele/CompteLivret.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Banque
+{
+    [Serializable]
+    class CompteLivret : Compte
+    {
+        double _taux;
+
+        public double Taux
+        {
+            get { return _taux; }
+            set { _taux = value; }
+        }
+
+        public CompteLivret(int n, Client c, double taux)
+            : base(n, c)
+        {
+            this._taux = taux;
+        }
+
+        public override string Description
+        {
+            get { return base.Description + " taux : " + this._taux; }
+        }
+
+
+    }
+}
